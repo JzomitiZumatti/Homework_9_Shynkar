d01b2fee41896afd1f9358624344fd64217c02a8
JzomitiZumatti <evgeniyshinkar@gmail.com>
2023-08-14 12:40:31 +0300

    Remove redundant do-while loop.

--- Src/Shuttle.java
+++ Src/Shuttle.java
@@ -4,22 +4,22 @@ public class Shuttle {
     final static int a = 4; //forbidden number
     final static int b = 9; //forbidden number
     final static int numOfShuttles = 100; //general number of shuttles
+
     public static void main(String[] args) {
         int counter = 1;
         int endFor = 101; // because counter value is equal to 1, endFor - counter must be equal to numOfShuttles
         ArrayList<Integer> numbers = new ArrayList<>();
-        do {
-            for (int i = 1; i < endFor; i++) {
-                if (i / 100 == a || i / 10 % 10 == a || i % 10 == a ||
-                        i / 100 == b || i / 10 % 10 == b || i % 10 == b) {
-                    endFor++;
-                } else {
-                    numbers.add(i);
-                    System.out.print(i + " ");
-                    counter++;
-                }
+
+        for (int i = 1; i < endFor; i++) {
+            if (i / 100 == a || i / 10 % 10 == a || i % 10 == a ||
+                    i / 100 == b || i / 10 % 10 == b || i % 10 == b) {
+                endFor++;
+            } else {
+                numbers.add(i);
+                System.out.print(i + " ");
+                counter++;
             }
-        } while (counter < numOfShuttles);
+        }
         //you can display shuttle numbers in 10 lines with 10 numbers in each, just uncomment the code below
         /*System.out.println("\n");
         counter = 0;
